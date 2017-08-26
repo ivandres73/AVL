a6dc21e9388113d1d0d76970c98f9e8ba3790d32
Ivan Carcamo <ivandres73@unitec.edu>
2017-08-25 23:54:07 -0600

    Agregados DataFile

---
+++ DataFile.cpp
@@ -0,0 +1,46 @@
+#include "DataFile.h"
+
+DataFile::DataFile()
+{
+    //ctor
+}
+
+DataFile::DataFile(char* path)
+{
+    DataFile::path = path;
+}
+
+DataFile::~DataFile()
+{
+    //dtor
+}
+
+void DataFile::abrir(char* proposito)
+{
+    archivo = fopen(path, proposito);
+}
+
+void DataFile::cerrar()
+{
+    fclose(archivo);
+}
+
+void DataFile::escribir(char* datos, int pos, int longitud)
+{
+    fseek(archivo, pos, SEEK_SET);
+        fwrite(datos, sizeof(char), longitud, archivo);
+}
+
+char* DataFile::leer(int pos, int longitud)
+{
+    char* datos = new char[longitud];
+    fseek(archivo, pos, SEEK_SET);
+    fread(datos , sizeof(char), longitud, archivo);
+    return datos;
+}
+
+int DataFile::longitudArchivo()
+{
+    //fseek(archivo, 0, SEEK_END);
+    //return sizeof(0);
+}
